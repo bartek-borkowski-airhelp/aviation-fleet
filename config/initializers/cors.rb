1d16454ce59fcc6f8467817242daa262b8bba065
bartek-borkowski-airhelp <bartosz.borkowski@airhelp.com>
2026-04-30 14:36:06 +0200

    Skip CORS initializer in test env to avoid missing UI_ORIGIN

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV.fetch("UI_ORIGIN")
+unless Rails.env.test?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV.fetch("UI_ORIGIN")
 
-    resource "*",
-      headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      resource "*",
+        headers: :any,
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+    end
   end
 end

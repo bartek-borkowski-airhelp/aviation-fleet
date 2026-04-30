23af0cd639ee60fd84ca1736cd7aaa072d875c30
bartek-borkowski-airhelp <bartosz.borkowski@airhelp.com>
2026-04-30 15:00:06 +0200

    Fix Basic Auth: include HttpAuthentication::Basic::ControllerMethods
    ActionController::API does not include Basic auth helpers by default
    (unlike ActionController::Base). Explicitly including the module
    makes http_basic_authenticate_with available as a class method.
    
    Also reverts the incorrect bootsnap workaround - the FrozenError was
    a symptom of the NoMethodError aborting Rails boot mid-initialisation,
    not a bootsnap issue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+
   http_basic_authenticate_with name: ENV.fetch("API_USERNAME", "admin"),
                                 password: ENV.fetch("API_PASSWORD", "password")
 end

c1f90ecbee4025fd42a5f3db950ffe9feeb91124
bartek-borkowski-airhelp <bartosz.borkowski@airhelp.com>
2026-04-30 15:43:38 +0200

    Add propeller engine_type and biplane body_type enum values
    - Add propeller to AircraftVariant engine_type enum
    - Add biplane to AircraftVariant body_type enum
    - Update rswag component schemas (AircraftVariant, AircraftVariantInput)
    - Update aircraft_variants_spec query parameter enums
    - Regenerate swagger/v1/swagger.yaml

--- app/models/aircraft_variant.rb
+++ app/models/aircraft_variant.rb
@@ -3,8 +3,8 @@ class AircraftVariant < ApplicationRecord
   has_many :fleet_entries, dependent: :destroy
   has_many :airlines, through: :fleet_entries
 
-  enum :engine_type, { turbofan: "turbofan", turboprop: "turboprop", piston: "piston" }
-  enum :body_type, { narrowbody: "narrowbody", widebody: "widebody", regional_jet: "regional_jet" }
+  enum :engine_type, { turbofan: "turbofan", turboprop: "turboprop", piston: "piston", propeller: "propeller" }
+  enum :body_type, { narrowbody: "narrowbody", widebody: "widebody", regional_jet: "regional_jet", biplane: "biplane" }
   enum :range_category, { short: "short", medium: "medium", long: "long" }
   enum :status, { active: "active", discontinued: "discontinued", in_development: "in_development" }
 
